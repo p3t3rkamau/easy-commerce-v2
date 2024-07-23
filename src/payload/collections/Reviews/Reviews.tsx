09d7d0c8540304caa77346a78b561baed2f1324e
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-07-23 22:53:25 +0300

    add:review collection

--- src/payload/collections/Reviews/Reviews.tsx
+++ src/payload/collections/Reviews/Reviews.tsx
@@ -1,4 +1,4 @@
-import type { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from 'payload/types'
 
 const Reviews: CollectionConfig = {
   slug: 'Reviews',
@@ -20,86 +20,50 @@ const Reviews: CollectionConfig = {
       hooks: {
         beforeChange: [
           ({ data }) => {
-            if (data.name && data.rating) {
-              data.title = `${data.name} - Rating: ${data.rating}`
+            if (data.product && data.rating) {
+              data.title = `${data.product} - Rating: ${data.rating}`
             }
           },
         ],
       },
     },
     {
-      name: 'user',
+      name: 'product',
       type: 'relationship',
-      relationTo: 'users',
+      relationTo: 'products',
       required: true,
-      hasMany: false,
-      admin: {
-        condition: () => false,
-      },
-    },
-    {
-      name: 'name',
-      label: 'Name',
-      type: 'text',
-      required: false,
-    },
-    {
-      name: 'message',
-      label: 'Review',
-      type: 'textarea',
-      required: false,
     },
     {
       name: 'rating',
-      label: 'Rating',
-      type: 'radio',
-      required: true,
+      type: 'select',
       options: [
-        {
-          value: '0',
-          label: '0.0',
-        },
-        {
-          value: '0.5',
-          label: '0.5',
-        },
-        {
-          value: '1',
-          label: '1.0',
-        },
-        {
-          value: '1.5',
-          label: '1.5',
-        },
-        {
-          value: '2',
-          label: '2.0',
-        },
-        {
-          value: '2.5',
-          label: '2.5',
-        },
-        {
-          value: '3',
-          label: '3.0',
-        },
-        {
-          value: '3.5',
-          label: '3.5',
-        },
-        {
-          value: '4',
-          label: '4.0',
-        },
-        {
-          value: '4.5',
-          label: '4.5',
-        },
-        {
-          value: '5',
-          label: '5.0',
-        },
+        { label: '1 Star', value: '1' },
+        { label: '2 Stars', value: '2' },
+        { label: '3 Stars', value: '3' },
+        { label: '4 Stars', value: '4' },
+        { label: '5 Stars', value: '5' },
       ],
+      required: true,
+    },
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'author',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'verifiedPurchase',
+      type: 'checkbox',
+      defaultValue: true,
     },
   ],
 }
