1d959a297802a638b58f031be23d896498b72f52
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-25 17:25:17 +0300

    add: calculate price in price componet

--- src/app/_components/Price/index.tsx
+++ src/app/_components/Price/index.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
 
-import { Post, Product } from '../../../payload/payload-types'
+import { Product } from '../../../payload/payload-types'
 
 import classes from './index.module.scss'
 
 export const Price: React.FC<{
-  product: Product | Post
+  product: Product
   quantity?: number
   button?: 'addToCart' | 'removeFromCart' | false
+  selectedAttributes?: { [key: string]: { value: string; quantity: number } }
 }> = props => {
-  const { product, quantity = 1, button } = props
+  const { product, quantity = 1, button, selectedAttributes } = props
+
+  const calculatePrice = () => {
+    let basePrice = product.price || 0
+    let attributesPrice = 0
+
+    if (selectedAttributes) {
+      Object.entries(selectedAttributes).forEach(([attr, { quantity }]) => {
+        const attributePrice = product.attributePrices?.[attr] || 0
+        attributesPrice += (attributePrice || 0) * quantity
+      })
+    }
+
+    return basePrice + attributesPrice
+  }
 
   const formatPrice = (amount: number) => {
-    return `${amount}`
+    return `${amount.toFixed(2)}`
   }
 
   return (
     <div className={classes.price}>
-      <span className={classes.amount}>Ksh{formatPrice(product.price * quantity)}</span>
+      <span className={classes.amount}>Ksh{formatPrice(calculatePrice() * quantity)}</span>
       {button && (
         <button className={classes[button]}>
           {button === 'addToCart' ? 'Add to Cart' : 'Remove from Cart'}
