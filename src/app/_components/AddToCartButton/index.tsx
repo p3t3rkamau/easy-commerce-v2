05092010b4c34b28ae050c3e4d41bbf0b85133c8
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-25 17:24:46 +0300

    delete: remove showmodal in add to cart button since it had no use being there

--- src/app/_components/AddToCartButton/index.tsx
+++ src/app/_components/AddToCartButton/index.tsx
@@ -1,16 +1,16 @@
+'use client'
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { Post, Product } from '../../../payload/payload-types'
 import { useCart } from '../../_providers/Cart'
-import AttributeModal from '../AttributesModal'
 import { Button, Props } from '../Button'
 
 import classes from './index.module.scss'
 
 export const AddToCartButton: React.FC<{
   product: Product | Post
-  quantity?: number
+  quantity: number
   className?: string
   appearance?: Props['appearance']
   selectedAttributes?: { [key: string]: { value: string; quantity: number }[] }
@@ -20,16 +20,11 @@ export const AddToCartButton: React.FC<{
   quantity = 1,
   className,
   appearance = 'primary',
-  selectedAttributes: initialSelectedAttributes,
+  selectedAttributes,
   attributePrices,
 }) => {
   const { cart, addItemToCart, isProductInCart, hasInitializedCart } = useCart()
   const [isInCart, setIsInCart] = useState<boolean>(false)
-  const [showModal, setShowModal] = useState<boolean>(false)
-  const [selectedAttributes, setSelectedAttributes] = useState<{
-    [key: string]: { value: string; quantity: number }[]
-  }>(initialSelectedAttributes || {})
-  const [currentQuantity, setCurrentQuantity] = useState<number>(quantity)
   const router = useRouter()
 
   useEffect(() => {
@@ -37,7 +32,8 @@ export const AddToCartButton: React.FC<{
   }, [isProductInCart, product, cart])
 
   const handleAddToCart = () => {
-    const validatedQuantity = Math.max(1, currentQuantity) // Ensure quantity is at least 1
+    const validatedQuantity = Math.max(1, quantity)
+    const storedTotalPrice = JSON.parse(localStorage.getItem('totalPrice') || '0')
 
     // Check if all required attributes are selected
     if (product.ProductsAttributes && product.ProductsAttributes.length > 0) {
@@ -45,10 +41,11 @@ export const AddToCartButton: React.FC<{
         attribute => selectedAttributes && selectedAttributes[attribute.Attribute_Name]?.length > 0,
       )
 
-      // if (!allAttributesSelected) {
-      //   setShowModal(true)
-      //   return
-      // }
+      // Prevent adding to cart if attributes are not selected
+      if (!allAttributesSelected) {
+        alert('Please select all required attributes before adding to cart.')
+        return
+      }
     }
 
     if (!isInCart) {
@@ -56,55 +53,33 @@ export const AddToCartButton: React.FC<{
         product,
         quantity: validatedQuantity,
         selectedAttributes,
-        attributePrices, // Pass the attribute prices here
+        attributePrices: { totalPrice: storedTotalPrice }, // Update with stored total price
       })
-      router.push('/cart')
+      router.push('/cart') // Navigate to the cart after adding
     }
   }
 
   const isOutOfStock = product.OutOfStock
 
   return (
-    <>
-      <Button
-        href={isInCart ? '/cart' : undefined}
-        type={!isInCart ? 'button' : undefined}
-        label={isOutOfStock ? 'Out of Stock' : isInCart ? `✓ View in cart` : `Add to cart`}
-        el={isInCart ? 'link' : undefined}
-        appearance={appearance}
-        className={[
-          className,
-          classes.addToCartButton,
-          isInCart && classes.green,
-          !hasInitializedCart && classes.hidden,
-          isOutOfStock ? classes.disabledButton : '',
-        ]
-          .filter(Boolean)
-          .join(' ')}
-        onClick={handleAddToCart}
-        disabled={isOutOfStock}
-      />
-      {showModal && (
-        <AttributeModal
-          product={product}
-          selectedAttributes={selectedAttributes}
-          setSelectedAttributes={setSelectedAttributes}
-          quantity={currentQuantity}
-          setQuantity={setCurrentQuantity}
-          onClose={() => setShowModal(false)}
-          onAddToCart={() => {
-            addItemToCart({
-              product,
-              quantity: currentQuantity,
-              selectedAttributes,
-              attributePrices, // Pass the attribute prices here
-            })
-            setShowModal(false)
-            router.push('/cart')
-          }}
-        />
-      )}
-    </>
+    <Button
+      href={isInCart ? '/cart' : undefined}
+      type={!isInCart ? 'button' : undefined}
+      label={isOutOfStock ? 'Out of Stock' : isInCart ? `✓ View in cart` : `Add to cart`}
+      el={isInCart ? 'link' : undefined}
+      appearance={appearance}
+      className={[
+        className,
+        classes.addToCartButton,
+        isInCart && classes.green,
+        !hasInitializedCart && classes.hidden,
+        isOutOfStock ? classes.disabledButton : '',
+      ]
+        .filter(Boolean)
+        .join(' ')}
+      onClick={handleAddToCart}
+      disabled={isOutOfStock}
+    />
   )
 }
 
