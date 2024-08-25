64e11fc663b8fb07e5439ba48dbbcb58267dc472
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-25 14:58:37 +0300

    delete: music tab

--- src/app/NextUi_components/tabs.tsx
+++ src/app/NextUi_components/tabs.tsx
@@ -21,12 +21,6 @@ const TabsUi: React.FC<TabsUiProps> = ({ description }) => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
     {
-      id: 'music',
-      label: 'Music',
-      content:
-        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    },
-    {
       id: 'videos',
       label: 'Videos',
       content:
