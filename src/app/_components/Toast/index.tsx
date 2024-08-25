f37d416b718c717562a8ea0fdce4352e51ceaaf0
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-25 17:27:33 +0300

    add: global toast to send notification and other messages

---
+++ src/app/_components/Toast/index.tsx
@@ -0,0 +1,21 @@
+// Toast.tsx
+import React, { useEffect } from 'react'
+
+import classes from './index.module.scss' // Import your SCSS file
+
+interface ToastProps {
+  message: string
+  onClose: () => void
+}
+
+const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(onClose, 3000) // Toast disappears after 3 seconds
+
+    return () => clearTimeout(timer) // Cleanup timer on unmount
+  }, [onClose])
+
+  return <div className={classes.toast}>{message}</div>
+}
+
+export default Toast
