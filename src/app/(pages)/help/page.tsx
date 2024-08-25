1d560c98fc12ea59c9edae957c4145635f4697d1
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-25 17:29:52 +0300

    add: test button for toast in help page

--- src/app/(pages)/help/page.tsx
+++ src/app/(pages)/help/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Metadata } from 'next'
 
+import { useToast } from '../../../app/_providers/Toast/ToastContext'
 import { Gutter } from '../../_components/Gutter'
 import HelpCenter from '../../_components/HelpPage/index'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
@@ -8,9 +9,15 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import classes from './index.module.scss'
 
 export default async function Help() {
+  const { addToast } = useToast()
+
+  const showToast = () => {
+    addToast('This is a toast message!')
+  }
   return (
     <Gutter className={classes.logout}>
       <HelpCenter />
+      <button onClick={showToast}>Show Toast</button>
     </Gutter>
   )
 }
