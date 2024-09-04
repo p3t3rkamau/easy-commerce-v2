f002806eccc0c5a5e8c2dd494c3f3f41637c4f6d
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-09-04 05:18:26 +0300

    add: error boundary to catch global errors

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -1,24 +1,24 @@
 'use client'
-
 import React from 'react'
 import { NextUIProvider } from '@nextui-org/react'
 
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
+import ErrorBoundary from './ErrorBoundary' // Import the functional ErrorBoundary
 import { FilterProvider } from './Filter'
 import { ThemeProvider } from './Theme'
 import { ToastProvider } from './Toast/ToastContext'
 
-export const Providers: React.FC<{
-  children: React.ReactNode
-}> = ({ children }) => {
+export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <ThemeProvider>
       <ToastProvider>
         <NextUIProvider>
           <AuthProvider>
             <FilterProvider>
-              <CartProvider>{children}</CartProvider>
+              <CartProvider>
+                <ErrorBoundary> {children}</ErrorBoundary>
+              </CartProvider>
             </FilterProvider>
           </AuthProvider>
         </NextUIProvider>
