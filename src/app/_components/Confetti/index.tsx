7346f3aa7fd35aff4af991dbd8c24eea776a4933
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-09-15 21:55:01 +0300

    add: react confettii effects on succesful order

--- src/app/_components/Confetti/index.tsx
+++ src/app/_components/Confetti/index.tsx
@@ -1,18 +1,18 @@
-'use client';
+'use client'
 
-import React, { useEffect, useState } from 'react';
-import Confetti from 'react-confetti';
+import React, { useEffect, useState } from 'react'
+import Confetti from 'react-confetti'
 
 const ConfettiEffect: React.FC = () => {
-  const [showConfetti, setShowConfetti] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false)
 
   useEffect(() => {
-    setShowConfetti(true); // Trigger the confetti effect when the component mounts
-    const timer = setTimeout(() => setShowConfetti(false), 5000); // Stop confetti after 5 seconds
-    return () => clearTimeout(timer); // Cleanup the timer on unmount
-  }, []);
+    setShowConfetti(true) // Trigger the confetti effect when the component mounts
+    const timer = setTimeout(() => setShowConfetti(false), 5000) // Stop confetti after 5 seconds
+    return () => clearTimeout(timer) // Cleanup the timer on unmount
+  }, [])
 
-  return showConfetti ? <Confetti /> : null;
-};
+  return showConfetti ? <Confetti /> : null
+}
 
-export default ConfettiEffect;
+export default ConfettiEffect
